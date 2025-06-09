6d1cb3ab51ec2646b29aa6ba6acf1b84defe45fd
Hermínio Tasinafo Honório <h.tasinafohonorio@tudelft.nl>
2025-06-09 13:26:30 +0200

    Tutorial_1 is working fine.

--- examples/tutorial_1/plot_results.py
+++ examples/tutorial_1/plot_results.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.join("..", "..", "safeincave"))
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from ResultsHandler import read_vector_from_points
+from ResultsHandler import read_vector_from_points, read_mesh_as_pandas
 
 def apply_grey_theme(fig, axes, transparent=True, grid_color="0.92", back_color='0.85'):
 	fig.patch.set_facecolor("#212121ff")
@@ -24,20 +24,25 @@ def apply_grey_theme(fig, axes, transparent=True, grid_color="0.92", back_color=
 			ax.xaxis.label.set_color('black')
 			ax.set_facecolor(back_color)
 
-# Read displacement results
-pvd_path = os.path.join("output", "case_0", "operation", "vtk", "displacement")
-pvd_file = "displacement.pvd"
-df_coord, u, v, w = read_vector_from_points(pvd_path, pvd_file)
 
-point_A = df_coord[(df_coord["z"] == 1) & (df_coord["x"] == 0) & (df_coord["y"] == 0)].index[0]
-point_B = df_coord[(df_coord["z"] == 1) & (df_coord["x"] == 0) & (df_coord["y"] == 1)].index[0]
-point_C = df_coord[(df_coord["z"] == 1) & (df_coord["x"] == 1) & (df_coord["y"] == 1)].index[0]
-point_D = df_coord[(df_coord["z"] == 1) & (df_coord["x"] == 1) & (df_coord["y"] == 0)].index[0]
+# Define results file
+file_name = os.path.join("output", "case_0", "operation", "vtk", "displacement", "u.xdmf")
+
+# Read mesh
+df_points, df_cells = read_mesh_as_pandas(file_name)
+
+# Read displacements
+u, v, w = read_vector_from_points(file_name)
+
+point_A = df_points[(df_points["z"] == 1) & (df_points["x"] == 0) & (df_points["y"] == 0)].index[0]
+point_B = df_points[(df_points["z"] == 1) & (df_points["x"] == 0) & (df_points["y"] == 1)].index[0]
+point_C = df_points[(df_points["z"] == 1) & (df_points["x"] == 1) & (df_points["y"] == 1)].index[0]
+point_D = df_points[(df_points["z"] == 1) & (df_points["x"] == 1) & (df_points["y"] == 0)].index[0]
 print(point_A, point_B, point_C, point_D)
-print("Point A: ", df_coord.iloc[point_A].values)
-print("Point B: ", df_coord.iloc[point_B].values)
-print("Point C: ", df_coord.iloc[point_C].values)
-print("Point D: ", df_coord.iloc[point_D].values)
+print("Point A: ", df_points.iloc[point_A].values)
+print("Point B: ", df_points.iloc[point_B].values)
+print("Point C: ", df_points.iloc[point_C].values)
+print("Point D: ", df_points.iloc[point_D].values)
 
 w_A = w.iloc[point_A].values[1:]
 w_B = w.iloc[point_B].values[1:]
