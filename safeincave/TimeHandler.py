310de8d771bd794c15f526bcd880cf5608de6b9c
Hermínio Tasinafo Honório <h.tasinafohonorio@tudelft.nl>
2025-08-12 15:55:10 +0200

    Implemented base abstract class for TimeHandler.py

--- safeincave/TimeHandler.py
+++ safeincave/TimeHandler.py
@@ -16,13 +16,13 @@ Classes responsible for time management (time stepping, controlling final time, 
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
 # License for the specific language governing permissions and limitations under
 # the License.
+from abc import ABC, abstractmethod
 from Utils import minute, hour, day, year
 import numpy as np
 
-class TimeController():
-	def __init__(self, time_step, final_time, initial_time, time_unit="second"):
+class TimeControllerBase(ABC):
+	def __init__(self, initial_time, final_time, time_unit="second"):
 		self.__decide_time_unit(time_unit)
-		self.dt = time_step*self.time_unit
 		self.t_final = final_time*self.time_unit
 		self.t_initial = initial_time*self.time_unit
 		self.t = initial_time*self.time_unit
@@ -41,40 +41,31 @@ class TimeController():
 		else:
 			raise Exception(f"Time unit {time_unit} not supported.")
 
-	def advance_time(self):
-		self.t += self.dt
-
 	def keep_looping(self):
 		return self.t < self.t_final
 
+	@abstractmethod
+	def advance_time(self):
+		pass
+
+
+
+class TimeController(TimeControllerBase):
+	def __init__(self, time_step, initial_time, final_time, time_unit="second"):
+		super().__init__(initial_time, final_time, time_unit)
+		self.dt = time_step*self.time_unit
+
+	def advance_time(self):
+		self.t += self.dt
+
+
 
 class TimeControllerParabolic():
-	def __init__(self, final_time, initial_time, n_time_steps, time_unit="second"):
-		self.__decide_time_unit(time_unit)
+	def __init__(self, n_time_steps, initial_time, final_time, time_unit="second"):
+		super().__init__(initial_time, final_time, time_unit)
 		self.n_time_steps = n_time_steps
-		self.t_initial = initial_time*self.time_unit
-		self.t_final = final_time*self.time_unit
-
 		self.time_list = self.calculate_varying_times(self.fun_parabolic)
-		self.time_step = 0
-		self.t = self.time_list[self.time_step]
-		# self.dt = self.time_list[1] - self.time_list[0]
-		self.dt = 0
-
-	def __decide_time_unit(self, time_unit):
-		if time_unit == "second":
-			self.time_unit = 1
-		elif time_unit == "minute":
-			self.time_unit = minute
-		elif time_unit == "hour":
-			self.time_unit = hour
-		elif time_unit == "day":
-			self.time_unit = day
-		elif time_unit == "year":
-			self.time_unit = year
-		else:
-			raise Exception(f"Time unit {time_unit} not supported.")
-
+		self.dt = self.time_list[1] - self.time_list[0]
 
 	def fun_parabolic(self, t_array):
 		return t_array**2
@@ -92,8 +83,4 @@ class TimeControllerParabolic():
 		self.time_step += 1
 		self.t = self.time_list[self.time_step]
 		self.dt = self.time_list[self.time_step] - self.time_list[self.time_step-1]
-
-	def keep_looping(self):
-		return self.t < self.t_final
-
 
