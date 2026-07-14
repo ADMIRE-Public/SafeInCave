7241f541e73a9e1877954c296a385f27c59d0698
drdtud <d.damasceno@tudelft.nl>
2026-07-14 21:45:05 +0200

    removed M_out

--- safeincave/Simulation/Simulators/__init__.py
+++ safeincave/Simulation/Simulators/__init__.py
@@ -8,12 +8,10 @@ from .base import Simulator  # noqa: F401
 from .thermo_mechanical import Simulator_TM  # noqa: F401
 from .mechanical import Simulator_M  # noqa: F401
 from .thermal import Simulator_T  # noqa: F401
-from .mechanical_out import Simulator_Mout  # noqa: F401
 
 __all__ = [
     "Simulator",
     "Simulator_TM",
     "Simulator_M",
     "Simulator_T",
-    "Simulator_Mout",
 ]
